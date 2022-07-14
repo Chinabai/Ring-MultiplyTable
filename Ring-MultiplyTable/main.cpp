e9a9da7d2aee7db1db3fd9ca7da072ca1a28e356
Chinabai <Chinabai@gmail.com>
2022-07-14 18:44:34 +0800

    Initial Commit

---
+++ Ring-MultiplyTable/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ring-MultiplyTable
+//
+//  Created by 李萍萍 on 2022/7/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
